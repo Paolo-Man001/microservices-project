bdf22b8108a1409cf18ac3e6d4510a0a60ede58d
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-28 17:47:07 +1200

    Create ZUUL API gateway microservice

---
+++ photoapp-zuul-api-gateway/src/test/java/com/paolomanlunas/photoappzuulapigateway/PhotoappZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paolomanlunas.photoappzuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappZuulApiGatewayApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
