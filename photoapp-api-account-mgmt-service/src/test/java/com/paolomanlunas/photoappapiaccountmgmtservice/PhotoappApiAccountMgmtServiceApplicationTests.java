a1d48eb43275db5371ffe94270668cebb9199564
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-28 17:20:34 +1200

    Create Account-Management Microservice

---
+++ photoapp-api-account-mgmt-service/src/test/java/com/paolomanlunas/photoappapiaccountmgmtservice/PhotoappApiAccountMgmtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paolomanlunas.photoappapiaccountmgmtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappApiAccountMgmtServiceApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
