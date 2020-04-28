6f0ac0740df1246130879eb4260e1c579d25e949
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-28 21:38:48 +1200

    s8-54 : Load-Balancer Demo :   Enable the 'Allow Parallel Run' in Configuration of 'ApiUserService' to have mulitple instances running.   In 'ApiUserService' Controller, add Springboot's Environment variable to access app-port-numbers.   Access and Display port numbers when '/users/status/check' url is accessed. Run multiple instances.   In browser, open Eureka. Check different instances via Zuul API gateway. Refresh and see multiple PORT numbers running.

--- photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiuserservice/users/ui/controllers/UsersController.java
+++ photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiuserservice/users/ui/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.paolomanlunas.photoappapiuserservice.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+   // Get access to Environment variable:
+   private final Environment env;
+
+   @Autowired
+   public UsersController(Environment env) {
+      this.env = env;
+   }
+
    @GetMapping("/status/check")
    public String status() {
-      return "Users API Service is Working...";
+      return "Users API Service is Working on PORT: " + env.getProperty("local.server.port");
    }
 }
