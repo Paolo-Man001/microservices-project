cae4e7fb18489e00b2d870c392ebccca0febecd5
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-01 22:49:43 +1200

    s10-59-61 : Create 'CreateUserRequestModel' :   Create CreateUserRequestModel inside new package '...users.ui.model'.   Create PostMapping 'createUser' in Controller and pass @RequestBody CreateUserRequestModel-type arg.

--- photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiuserservice/users/ui/controllers/UsersController.java
+++ photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiuserservice/users/ui/controllers/UsersController.java
@@ -1,10 +1,9 @@
 package com.paolomanlunas.photoappapiuserservice.users.ui.controllers;
 
+import com.paolomanlunas.photoappapiuserservice.users.ui.model.CreateUserRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -21,5 +20,10 @@ public class UsersController {
    @GetMapping("/status/check")
    public String status() {
       return "Users API Service is Working on PORT: " + env.getProperty("local.server.port");
+   }
+
+   @PostMapping()
+   public String createUser(@RequestBody CreateUserRequestModel userDetails) {
+      return "Create user method is called.";
    }
 }
