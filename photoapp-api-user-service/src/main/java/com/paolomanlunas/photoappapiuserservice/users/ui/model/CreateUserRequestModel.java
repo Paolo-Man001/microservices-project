a00633900b393affeb791604e24b074f344683a9
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-01 22:59:22 +1200

    s10-61 : Validation for 'CreateUserRequestModel' :   Add Validation-Annotations on 'CreateUserRequestModel-Class'.

--- photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiuserservice/users/ui/model/CreateUserRequestModel.java
+++ photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiuserservice/users/ui/model/CreateUserRequestModel.java
@@ -1,9 +1,24 @@
 package com.paolomanlunas.photoappapiuserservice.users.ui.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CreateUserRequestModel {
+   @NotNull(message = "First Name cannot be empty.")
+   @Size(min = 2, message = "First Name must be at least 2 characters.")
    private String firstName;
+
+   @NotNull(message = "Last Name cannot be empty.")
+   @Size(min = 2, message = "First Name must be at least 2 characters.")
    private String lastName;
+
+   @NotNull(message = "Password cannot be empty.")
+   @Size(min = 8, max = 16, message = "Password must be at least 8, but not more than 16 characters.") // number-Constraint for password characters
    private String password;
+
+   @NotNull(message = "Email cannot be empty.")
+   @Email
    private String email;
 
    // GET-SET
