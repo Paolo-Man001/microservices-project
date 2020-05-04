cd53bdfb69331de45652f85a16c14a250b0ded9f
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-04 23:58:04 +1200

    s10-65 : Generate Unique public UserId :   Create userId by generating UUID.randomUUID() in UserServiceImpl's createUser().

--- photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/service/UserServiceImpl.java
+++ photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/service/UserServiceImpl.java
@@ -2,9 +2,15 @@ package com.paolomanlunas.photoappapiusers.service;
 
 import com.paolomanlunas.photoappapiusers.shared.UserDTO;
 
+import java.util.UUID;
+
 public class UserServiceImpl implements UserService {
    @Override
    public UserDTO createUser(UserDTO userDetails) {
+
+      // set a Unique UUID
+      userDetails.setUserId(UUID.randomUUID().toString());
+
       return null;
    }
 }
