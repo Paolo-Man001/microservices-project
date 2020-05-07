2b5f3fed0b5315a748d2626e626016e6d029f569
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-07 23:20:33 +1200

    s10-69-v4(fin): UserServiceImpl.createUser() :   Annotate @Service-UserServiceImpl and DI(@Autowire) UserServiceImpl inside UsersController.   Map (POST)RequestBody with ModelMapper and copy source(RequestBody) into DTO-object.   Call userService.createUser(DTO) to create-and-save New User into h2-DB.   Use PostMan to POST to '8011/users-ws/users'. debug-to-observe. open h2-console, then check DB.

--- photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/ui/controllers/UsersController.java
+++ photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/ui/controllers/UsersController.java
@@ -1,6 +1,10 @@
 package com.paolomanlunas.photoappapiusers.ui.controllers;
 
+import com.paolomanlunas.photoappapiusers.service.UserServiceImpl;
+import com.paolomanlunas.photoappapiusers.shared.UserDTO;
 import com.paolomanlunas.photoappapiusers.ui.model.CreateUserRequestModel;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +18,12 @@ public class UsersController {
    // Get access to Environment variable:
    private final Environment env;
 
+   private final UserServiceImpl userService;
+
    @Autowired
-   public UsersController(Environment env) {
+   public UsersController(Environment env, UserServiceImpl userService) {
       this.env = env;
+      this.userService = userService;
    }
 
    @GetMapping("/status/check")
@@ -26,6 +33,13 @@ public class UsersController {
 
    @PostMapping()
    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+
+      ModelMapper modelMapper = new ModelMapper();
+      modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+      UserDTO userDTO = modelMapper.map(userDetails, UserDTO.class);
+      userService.createUser(userDTO);
+
       return "Create user method is called.";
    }
 }
