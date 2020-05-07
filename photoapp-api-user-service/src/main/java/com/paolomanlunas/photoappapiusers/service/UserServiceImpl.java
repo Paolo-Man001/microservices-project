b8dab74ddee0785f2c5c40e093ad59fb48d1d370
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-07 22:23:46 +1200

    s10-69-v2: ModelMapper in UserServiceImpl:   use ModelMapper and setup the mapping between the source-object(UserDTO) and destination-object(UserEntity).   Set the ModelMapper Configuration for MatchingStrategy to match Properties/fields for both sourc + destination objects.

--- photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/service/UserServiceImpl.java
+++ photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/service/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.paolomanlunas.photoappapiusers.service;
 
+import com.paolomanlunas.photoappapiusers.data.UserEntity;
 import com.paolomanlunas.photoappapiusers.shared.UserDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 
 import java.util.UUID;
 
@@ -10,6 +14,11 @@ public class UserServiceImpl implements UserService {
 
       // set a Unique UUID
       userDetails.setUserId(UUID.randomUUID().toString());
+
+      // ModelMapper to map data to ModelDTO then copy to ModelEntity
+      ModelMapper modelMapper = new ModelMapper();
+      modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);   // ModelMapper MUST match the properties/fields in the destination-object(UserEntity)
+      UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);          // save into variable-typeof-UserEntity
 
       return null;
    }
