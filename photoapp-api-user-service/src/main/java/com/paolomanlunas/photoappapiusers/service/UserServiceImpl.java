13893e1be74bfde799798bd9a18d487063346f84
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-07 22:38:52 +1200

    s10-69-v3: UsersRepository :   @Autowire UsersRepository in the UserServiceImpl Constructor.   Use UsersRepository to persists and saves the object into the DB using the data in userEntity.   Hardcode (temporary) Encrypted-password for testing.

--- photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/service/UserServiceImpl.java
+++ photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/service/UserServiceImpl.java
@@ -1,14 +1,26 @@
 package com.paolomanlunas.photoappapiusers.service;
 
 import com.paolomanlunas.photoappapiusers.data.UserEntity;
+import com.paolomanlunas.photoappapiusers.data.UsersRepository;
 import com.paolomanlunas.photoappapiusers.shared.UserDTO;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+
+@Service
 public class UserServiceImpl implements UserService {
+
+   UsersRepository usersRepository;
+
+   @Autowired
+   public UserServiceImpl(UsersRepository usersRepository) {
+      this.usersRepository = usersRepository;
+   }
+
    @Override
    public UserDTO createUser(UserDTO userDetails) {
 
@@ -18,7 +30,14 @@ public class UserServiceImpl implements UserService {
       // ModelMapper to map data to ModelDTO then copy to ModelEntity
       ModelMapper modelMapper = new ModelMapper();
       modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);   // ModelMapper MUST match the properties/fields in the destination-object(UserEntity)
-      UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);          // save into variable-typeof-UserEntity
+      UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);          // map Data then copy into UserEntity-variable
+
+      /* TEMPORARY PASSWORD:
+      *     Encrypted Password is Temporary
+      * */
+      userEntity.setEncryptedPassword("test");
+
+      usersRepository.save(userEntity);   // save() method will take the Entity to persist into DB
 
       return null;
    }
