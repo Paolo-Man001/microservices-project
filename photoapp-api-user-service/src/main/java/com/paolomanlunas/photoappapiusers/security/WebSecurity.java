9203a6e9642deb6762638749b669903c786fdb61
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-12 13:11:43 +1200

    s10-75-76: n75 ONLY Allow Zuul API IP :   Modify WebSecurity Configfile to only allow IP-address of what Zull gataway is using(eureka's).   Keep the IP-address inside the application.properties file as "Key-Value".   DI Environment into WebSecurity-class to access IP from properties-file and reference the 'key' in WebSecurity Configfile.

--- photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/security/WebSecurity.java
+++ photoapp-api-user-service/src/main/java/com/paolomanlunas/photoappapiusers/security/WebSecurity.java
@@ -1,7 +1,9 @@
 package com.paolomanlunas.photoappapiusers.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,13 +13,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+   private Environment environment; // we use this to get 'gateway.ip' from application.properties
+
+   @Autowired
+   public WebSecurity(Environment environment) {
+      this.environment = environment;
+   }
+
    @Override
    protected void configure(HttpSecurity http) throws Exception {
       http.csrf().disable();
       http
               .authorizeRequests()
-              .antMatchers("/users/**")
-              .permitAll();
+              .antMatchers("/**")
+              .hasIpAddress(environment.getProperty("gateway.ip"));
 
       // Whiteframe - Disable to avoid whiteframe when connecting to h2-console
       http.headers().frameOptions().disable();
